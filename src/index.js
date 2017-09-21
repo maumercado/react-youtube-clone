794977ee26281813182be12c9ac822aa135a4cea
Mauricio Mercado <mau@maumercado.com>
2017-09-20 22:41:35 -0700

    Add Search Bar functionality, search state change on keystroke

--- src/index.js
+++ src/index.js
@@ -17,18 +17,23 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YouTubeSearch({ key: API_KEY, term: 'algorithms' }, videos => {
+        this.videoSearch('algorithms');
+    }
+
+    videoSearch(term) {
+        YouTubeSearch({ key: API_KEY, term: term }, videos => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
         });
+
     }
 
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
